8abf512008c522bb48bae9c8c4bc051276b83bda
Hope Tambala <hopetambala@outlook.com>
2020-08-09 19:27:01 -0400

    chore: fix styles

--- src/components/bioModule.js
+++ src/components/bioModule.js
@@ -2,7 +2,7 @@ import React from "react"
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
 
-import styles from "./bioModule.module.scss"
+// import styles from "./bioModule.module.scss"
 
 const BioModal = (props) => {
   return (
