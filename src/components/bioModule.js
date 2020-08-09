9edacecb0fa1c9ec48ab65584a1a2b5a8791ab36
Chance Murphy <46692350+chance-murphy@users.noreply.github.com>
2020-08-09 17:40:02 -0400

    bios done

---
+++ src/components/bioModule.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+
+import styles from "./bioModule.module.scss"
+
+const BioModal = (props) => {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton/>
+      <Modal.Body>
+        {props.children}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default BioModal
