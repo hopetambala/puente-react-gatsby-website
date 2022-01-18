7006bde17577bd2262ffb4c62439ac5cd3a65c35
Hope Tambala <hopetambala@outlook.com>
2022-01-18 14:03:07 -0700

    chore: add animation to donation tiers

--- src/pages/donate/index.js
+++ src/pages/donate/index.js
@@ -62,32 +62,37 @@ const DonatePage = () => {
               <img alt={data.contentfulDonationPage.logo.title} src={data.contentfulDonationPage.logo.resize.src} fluid />
               <div className={donateStyles.body}>
                 <div className={donateStyles.title}>
-                  <h1>{data.contentfulDonationPage.title}</h1>
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: data.contentfulDonationPage.pageText.childMarkdownRemark.html,
+                  <div className={donateStyles.content} dangerouslySetInnerHTML={{
+                        __html: data.contentfulDonationPage.monthlyDonorContent.childMarkdownRemark.html,
+                      }}
+                  /> 
+                  <div className={donateStyles.content} dangerouslySetInnerHTML={{
+                      __html: data.contentfulDonationPage.monthlyDonorHowItWorks.childMarkdownRemark.html,
                     }}
-                  />
+                  />                   
                 </div>
                 <div className={donateStyles.donateSection}>
                   <iframe title="donate" src="https://givebutter.com/embed/c/8vtwH6" className={donateStyles.donateForm} name="givebutter" frameborder="0" scrolling="no" seamless allowpaymentrequest />
                 </div>
               </div>
             </div>
-              <div className={donateStyles.bodyMonthly}>
-                <div className={donateStyles.content} dangerouslySetInnerHTML={{
-                      __html: data.contentfulDonationPage.monthlyDonorContent.childMarkdownRemark.html,
-                    }}
-                /> 
+            <div className={donateStyles.bodyMonthly} >
               <div className={donateStyles.tierImages}>
                 {data.contentfulDonationPage.monthlyDonorTiers.map((image)=>
                     <img alt={data.contentfulDonationPage.logo.title} src={image.resize.src} fluid />)
                 }
               </div>
-                <div className={donateStyles.content} dangerouslySetInnerHTML={{
-                    __html: data.contentfulDonationPage.monthlyDonorHowItWorks.childMarkdownRemark.html,
-                  }}
-                />                   
+            </div>
+            <div className={donateStyles.bodyMonthly}>
+              <div className={donateStyles.content}>
+                <h1>{data.contentfulDonationPage.title}</h1>
+              </div>
+              <div
+                className={donateStyles.content}
+                dangerouslySetInnerHTML={{
+                  __html: data.contentfulDonationPage.pageText.childMarkdownRemark.html,
+                }}
+              />
             </div>
           </div>
         </div>
