559098a26e15660cb0106b333c95984f43eeefd8
Hope Tambala <hopetambala@outlook.com>
2022-02-12 14:29:01 -0500

    chore: padding

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -35,10 +35,9 @@ const Header = () => {
 
   const [width] = useWindowSize();
 
-  console.log(width);
-
   if (width > 768) {
     return (
+      <>
       <Navbar className={styles.navBackground} collapseOnSelect expand="lg" onScroll>
         <Navbar.Brand className={styles.logo}>
           <Link to="/">
@@ -141,12 +140,6 @@ const Header = () => {
                 <NavDropdown.Item className={styles.dropdownItem}>
                   <Link style={{ color: "#000" }} to="/donate-monthly">Join our monthly donor program</Link>
                 </NavDropdown.Item>
-                {/* <NavDropdown.Item className={styles.dropdownItem}>
-                  <Link style={{ color: "#000" }} to="/volunteer#newsletter">Newsletter</Link>
-                </NavDropdown.Item>
-                <NavDropdown.Item className={styles.dropdownItem}>
-                  <Link style={{ color: "#000" }} to="/volunteer#donate">Donate</Link>
-                </NavDropdown.Item> */}
               </NavDropdown>
             </Nav.Item>
             <Nav.Item className={styles.nav}>
@@ -161,6 +154,7 @@ const Header = () => {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+      </>
     )
   } else {
     return (
@@ -218,91 +212,3 @@ const Header = () => {
 }
 
 export default Header
-
-// import React from "react"
-// import {
-//   Link,
-//   useStaticQuery,
-//   graphql
-// } from "gatsby"
-// import Img from "gatsby-image"
-// import Button from 'react-bootstrap/Button'
-
-// // Style / Icon Imports
-// import headerStyles from './header.module.scss'
-// import { Icon } from 'react-icons-kit'
-// import { phone } from 'react-icons-kit/fa/phone'
-// import { envelope } from 'react-icons-kit/fa/envelope'
-// import { bars } from 'react-icons-kit/fa/bars'
-// 
-// const Header = () => {
-//   const logo = useStaticQuery(graphql`
-//     query {
-//       logo: file(relativePath: { eq: "logo-black.png" }) {
-//         childImageSharp {
-//           fluid(maxWidth: 200) {
-//             ...GatsbyImageSharpFluid
-//           }
-//         }
-//       }
-//     }
-//   `)
-
-//   const handleClick = () => {
-//     // ReactDOM.findDOMNode.getElementsByClassName('navSection')
-//   }
-
-//   return (
-//     <div className={headerStyles.header}>
-//       <div className={headerStyles.headerMain}>
-//         <div className={headerStyles.logoSection}>
-//           <Link to="/">
-//             <Img className={headerStyles.logo} fluid={logo.logo.childImageSharp.fluid} />
-//           </Link>
-//         </div>
-//         <div className={headerStyles.navSection}>
-//           <div className={headerStyles.navContact}>
-//             <div>
-//               <Icon className={headerStyles.icon} size={20} icon={phone} />
-//               <p>1-574-302-7756</p>
-//             </div>
-//             <div>
-//               <Icon className={headerStyles.icon} size={20} icon={envelope} />
-//               <p>info@punete-dr.com</p>
-//             </div>
-//           </div>
-//           <div>
-//             <ul className={headerStyles.navList}>
-//               <li>
-//                 <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About Us</Link>
-//               </li>
-//               <li>
-//                 <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/technology">Our Technology</Link>
-//               </li>
-//               <li>
-//                 <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/programs">Programs</Link>
-//               </li>
-//               <li>
-//                 <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/volunteer">Volunteers</Link>
-//               </li>
-//               <li>
-//                 <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/news">News</Link>
-//               </li>
-//               <li>
-//                 <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/donate">
-//                   <Button className={headerStyles.buttonBackground}>
-//                     Donate
-//                   </Button>
-//                 </Link>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//         <div className={headerStyles.burger}>
-//           <Icon className={headerStyles.icon} onClick={handleClick} size={24} icon={bars} />
-//         </div>
-//       </div>
-//     </div> 
-//   )
-// }
-// export default Header
