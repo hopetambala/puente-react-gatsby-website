9aa73b031dd17aad823d2e02737636f110475e83
Hope Tambala <hopetambala@outlook.com>
2025-06-24 00:34:09 -0400

    fix: Can't resolve '../../components/DonateCTA' in '/opt/build/repo/src/pages/merchandise'

---
+++ src/components/DonateCTA/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import {
+  Link,
+} from "gatsby"
+import Button from 'react-bootstrap/Button'
+
+// Style Imports
+import * as styles from '../volunteerCTA/index.module.scss'
+
+const DonateCTA = () => {
+  return (
+    <div className={styles.container}>
+      <h2>You Can Also Support Puente by Donating!</h2>
+      <Link className={styles.button} to="/donate">
+        <Button className={styles.buttonBackground}>
+          Donate
+        </Button>
+      </Link>
+    </div>
+  )
+}
+
+export default DonateCTA
