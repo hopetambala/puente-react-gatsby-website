60d2025db0b9670db57e1dcad64e567aa05bbd14
Hope Tambala <hopetambala@outlook.com>
2025-06-24 00:25:03 -0400

    fix: weird naming

---
+++ src/components/partnerContactCTA/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+import Button from 'react-bootstrap/Button'
+
+// Style Imports
+import * as styles from './index.module.scss'
+
+// Component Imports
+import ContactModal from '../contactModule'
+
+const PartnerContactCTA = () => {
+  const [modalShow, setModalShow] = useState(false);
+  return (
+    <div className={styles.container}>
+      <h2>Ready to become a partner?</h2>
+      <div className={styles.button}>
+        <Button onClick={() => setModalShow(true)} className={styles.buttonBackground}>
+          Contact Us
+        </Button>
+        <ContactModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default PartnerContactCTA
