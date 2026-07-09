53b6411ab8ac56e9ff54e2d81208e9066878a368
Hope Tambala <hopetambala@gmail.com>
2026-07-09 13:10:31 -0700

    fix and add active tabs

--- src/components/subNav/index.js
+++ src/components/subNav/index.js
@@ -6,28 +6,13 @@ import * as styles from "./index.module.scss"
 const SubNav = () => (
   <nav className={styles.subNav} aria-label="The Puente Model">
     <div className={styles.inner}>
-      <Link
-        to="/model/"
-        className={styles.tab}
-        activeClassName={styles.tabActive}
-        getProps={({ isCurrent }) => (isCurrent ? { "aria-current": "page" } : {})}
-      >
+      <Link to="/model/" className={styles.tab} activeClassName={styles.tabActive}>
         The Puente Model
       </Link>
-      <Link
-        to="/model/projects/"
-        className={styles.tab}
-        activeClassName={styles.tabActive}
-        getProps={({ isCurrent }) => (isCurrent ? { "aria-current": "page" } : {})}
-      >
+      <Link to="/model/projects/" className={styles.tab} activeClassName={styles.tabActive}>
         Signature Projects
       </Link>
-      <Link
-        to="/model/health/"
-        className={styles.tab}
-        activeClassName={styles.tabActive}
-        getProps={({ isCurrent }) => (isCurrent ? { "aria-current": "page" } : {})}
-      >
+      <Link to="/model/health/" className={styles.tab} activeClassName={styles.tabActive}>
         Puente Health
       </Link>
     </div>
